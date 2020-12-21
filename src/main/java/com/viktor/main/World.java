000549b47705b7cb2485deb25f3a089dcd52520a
Víctor <victor.rodriguez@tcomet.com>
2020-12-21 09:57:07 +0100

    first commit

---
+++ src/main/java/com/viktor/main/World.java
@@ -0,0 +1,19 @@
+package com.viktor.main;
+
+public class World {
+	private long id;
+	private String message;
+
+	public World(long incrementAndGet, String format) {
+		this.id = incrementAndGet;
+		this.message = format;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+}
---
+++ src/test/java/com/viktor/pruebas/NunegalEntrevistaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viktor.pruebas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NunegalEntrevistaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
